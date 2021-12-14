d8e327bc8ea8acabb8288ce44c9e42b100c09c37
Pablo Perez <pablo.perez@corp.smtsoftware.com>
2021-12-14 10:35:39 -0300

    Se creo el archivo DockerFile

--- src/test/java/com/nubi/sistemaDeEncuestas/SistemaDeEncuestasApplicationTests.java
+++ src/test/java/com/nubi/sistemaDeEncuestas/SistemaDeEncuestasApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nubi.sistemaDeEncuestas;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nubi.sistemaDeEncuestas.services.Implementacion.PreguntaServiceImpl;
 
 @SpringBootTest
 class SistemaDeEncuestasApplicationTests {
+	
+	@Autowired
+	private PreguntaServiceImpl ps;
 
 	@Test
 	void contextLoads() {
+		
+	}
+	
+	@Test
+	void testGetPreguntasByUsuario() {
+		assertTrue(ps.getPreguntaByUsuario(1l).get(0).getTitulo().equalsIgnoreCase("esto es una pregunta"));
 	}
 
 }
